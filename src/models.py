73e7ca372affd87c951826dfe08f8d6cc9655c49
Mariabel17 <maribelml17@gmail.com>
2026-01-20 12:46:24 +0000

    tablas creadas

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column 
 
 db = SQLAlchemy()
 
@@ -17,3 +17,51 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planet(db.Model):
+
+    __table_name__ = "planet"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    diameter: Mapped[str] = mapped_column(String(120), nullable=False)
+    gravity: Mapped[str] = mapped_column(String(120), nullable=False)
+    climate: Mapped[str] = mapped_column(String(120), nullable=False)
+    terrain: Mapped[str] = mapped_column(String(120), nullable=False)
+
+
+
+class People(db.Model):
+
+    __table_name__ = "people"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(120),  nullable=False)
+    gender: Mapped[str] = mapped_column(String(120),  nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(120), nullable=False)
+    height: Mapped[int] = mapped_column( nullable=False)
+
+
+class Startship(db.Model):
+
+    __table_name__ = "startship"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    consumables: Mapped[str] = mapped_column(String(120),  nullable=False)
+    model: Mapped[str] = mapped_column(String(120),  nullable=False)
+    length: Mapped[int] = mapped_column(nullable=False)
+    passengers: Mapped[int] = mapped_column( nullable=False)
+    
+
+class Favorite(db.Model):
+
+    __table_name__ = "favorite"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id : Mapped[int] = mapped_column(ForeignKey("user.id"))
+    people_id :  Mapped[int] = mapped_column(ForeignKey("people.id"))
+    planet_id : Mapped[int] = mapped_column(ForeignKey("planet.id"))
+    startship_id : Mapped[int] = mapped_column(ForeignKey("startship.id"))
