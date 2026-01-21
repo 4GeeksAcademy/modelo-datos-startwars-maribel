4360a8636c1a40df3f43ead4d92e23c1231d0783
Mariabel17 <maribelml17@gmail.com>
2026-01-21 11:54:30 +0000

    Modelo fav n-n listo

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy import String, Boolean, ForeignKey, Table, Column
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
@@ -10,11 +10,10 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-    favorite_planets: Mapped[list["Planet"]] = relationship(secondary="favorite_planet", back_populates="fav_planet")
-    favorite_peoples: Mapped[list["People"]] = relationship(secondary="favorite_people", back_populates="fav_people")
-    favorite_starships: Mapped[list["Starship"]] = relationship(secondary="favorite_starship", back_populates="fav_starship")
-
+    favorite_planet: Mapped[list["Planet"]] = relationship(secondary = "planet_user", back_populates="planet")
+    favorite_people: Mapped[list["People"]] = relationship(secondary = "people_user", back_populates="people")
+    favorite_starship: Mapped[list["Starship"]] = relationship(secondary = "starship_user", back_populates="starship")
+    
 class Planet(db.Model):
 
     __tablename__ = "planet"
@@ -25,17 +24,9 @@ class Planet(db.Model):
     gravity: Mapped[str] = mapped_column(String(50), nullable=False)
     climate: Mapped[str] = mapped_column(String(30), nullable=False)
     terrain: Mapped[str] = mapped_column(String(100), nullable=False)
-    
 
-    fav_planet: Mapped[list["User"]] = relationship(secondary="favorite_planet", back_populates="favorite_planets")
+    users: Mapped[list["User"]] = relationship(secondary="favorite_planet",back_populates="favorite_planets")
 
-
-favorite_planet = db.Table(
-    "favorite_planet",
-    db.metadata, 
-    db.Column("planet_id", ForeignKey("planet.id"), primary_key=True),
-    db.Column("user_id", ForeignKey(("user.id")), primary_key=True)
-)
 
 class People(db.Model):
 
@@ -47,18 +38,8 @@ class People(db.Model):
     gender: Mapped[str] = mapped_column(String(120),  nullable=False)
     hair_color: Mapped[str] = mapped_column(String(120), nullable=False)
     height: Mapped[int] = mapped_column(nullable=False)
-
-    fav_people: Mapped[list["User"]] = relationship(secondary="favorite_people", back_populates="favorite_people")
-
-
-
-    favorite_people= db.Table(
-    "favorite_people",
-    db.metadata, 
-    db.Column("people_id", ForeignKey("people.id"), primary_key=True),
-    db.Column("user_id", ForeignKey(("user.id")), primary_key=True)
-)
-
+    
+    users: Mapped[list["User"]] = relationship(secondary="favorite_people",back_populates="favorite_people")
 
 class Starship(db.Model):
 
@@ -70,16 +51,16 @@ class Starship(db.Model):
     length: Mapped[int] = mapped_column(nullable=False)
     passengers: Mapped[int] = mapped_column(nullable=False)
 
+    users: Mapped[list["User"]] = relationship(secondary="favorite_starship",back_populates="favorite_starship")
+ 
 
-    fav_starship: Mapped[list["User"]] = relationship(secondary="favorite_starship", back_populates="favorite_starship")
-
-
-
-    favorite_starship= db.Table(
-    "favorite_starship",
-    db.metadata, 
-    db.Column("starship_id", ForeignKey("starship.id"), primary_key=True),
-    db.Column("user_id", ForeignKey(("user.id")), primary_key=True)
+favorites = Table(
+    "favorites",
+    db.metadata,
+    Column("planet_id", ForeignKey("planet.id")),
+    Column("user_id", ForeignKey("user.id")),
+    Column("people_id", ForeignKey("people.id")), 
+    Column("starship_id", ForeignKey("starship.id"))
 )
 
 
